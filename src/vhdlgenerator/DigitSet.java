a57302554a64b235194bc5c46cc8143f97a77919
Karol Gancarz <karol.gancarz10@imperial.ac.uk>
2014-06-01 03:41:03 +0100

    Added interactive mode and value check for r and n

--- src/vhdlgenerator/DigitSet.java
+++ src/vhdlgenerator/DigitSet.java
@@ -7,6 +7,7 @@
 package vhdlgenerator;
 
 import java.util.List;
+import java.util.ListIterator;
 import java.util.ArrayList;
 
 public class DigitSet {
@@ -14,7 +15,7 @@ public class DigitSet {
     // The digit set is of the form
     // D = {-a, ..., a} where a = r - 1
     
-    public final List<Integer> digits;
+    private final List<Integer> digits;
     public final int size;
     public final int a;
     
@@ -28,9 +29,21 @@ public class DigitSet {
     }
     
     public void printDigits () {
-        System.out.print("{");
-        for (Integer item : digits)
-            System.out.println();
+        System.out.println(this.toString());
+    }
+    
+    @Override
+    public String toString() {
+        String result = "{";
+        ListIterator<Integer> it = digits.listIterator();
+        while (it.hasNext()) {
+            result += it.next();
+            if (it.hasNext())
+                result += ", ";
+        }
+        result += "}";
+        
+        return result;
     }
     
 }
--- src/vhdlgenerator/VHDLGenerator.java
+++ src/vhdlgenerator/VHDLGenerator.java
@@ -7,12 +7,102 @@
 
 package vhdlgenerator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class VHDLGenerator {
+    
+    private static final String PROMPT = ">> ";
+    private static int r;                       // radix
+    private static int n;                       // precision
+    private static DigitSet D;                  // digit set
+    
+    private static void interactiveMode() {
+        printPrompt();
+        System.out.println("Enter radix (r >= 4) :");
+        r = getInteger();
+        
+        printPrompt();
+        System.out.println("Enter precision (n >= 1) :");
+        n = getInteger();
+    }
+    
+    private static void printPrompt() {
+        System.out.print(PROMPT);
+    }
+    
+    private static String getInput() {
+        String input;
+        
+        try {
+            InputStreamReader  consoleReader = new InputStreamReader(System.in);
+            BufferedReader reader = new BufferedReader(consoleReader);
+            
+            input = reader.readLine();
+            
+            /* You don't want to close the standard input stream.... */
+//            try (BufferedReader reader = new BufferedReader(consoleReader)) {
+//                input = reader.readLine();
+//            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        
+        return input;
+    }
+    
+    private static Integer getInteger() {
+        boolean obtained = false;
+        Integer result = 0;
+        
+        while (!obtained) {
+            try {
+                result = Integer.parseInt(getInput());
+                obtained = true;
+            } catch (NumberFormatException e) {
+                obtained = false;
+                printPrompt();
+                System.out.println("Try again:");
+            }
+        }
+        
+        return result;
+    }
 
     public static void main(String[] args) {
         
-        System.out.printf("%,6.2f", 124.000);
+        if (args.length == 0)
+            interactiveMode();
+        else if (args.length != 2) {
+            System.out.println("Usage: VHDLGenerator <radix> <precision>\n");
+            return;
+        }
+        else {
+            try {
+                r = Integer.parseInt(args[0]);
+                n = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid value for radix or precision\n");
+                return;
+            }
+        }
         
+        // Check correct values for r and n
+        if (r < 4) {
+            System.out.println("Radix has to be greater than or equal to 4\n");
+            return;
+        } else if (n < 1) {
+            System.out.println("Precision has to be at least 1\n");
+            return;
+        }
+        
+        D = new DigitSet(r);
+        printPrompt();
+        System.out.printf("Your radix is %d, precision is %d and your digit set is %n%s%s%n",
+                r, n, PROMPT, D);
+        
+        System.out.println();
     }
     
 }
